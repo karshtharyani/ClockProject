13a40ae9fd80ab6f3d502cca28896e976b9ddec1
ktharyan <ktharyan@mathworks.com>
2019-02-08 15:36:51 -0500

    started alarm

--- Clock.m
+++ Clock.m
@@ -9,14 +9,16 @@ t = timer;
 t.ExecutionMode = 'fixedSpacing';
 
 %% make the dial and all other static graphic and their properties
-[hFig, hAx, hToggle] = initializeGraphicHandles();
+[hFig, hAx, hToggle, hAlarm] = initializeGraphicHandles();
+alarmTime = {0, 0};
 
 [dial, dialText, dialCenter, dialTicks] = makeDial(hFig, hAx);
 secondHand = makeSecondHand(hFig, hAx);
 hourHand = makeHourHand(hFig, hAx);
 minuteHand = makeMinuteHand(hFig, hAx);
 hToggle.Callback = {@toggleTheme, ...
-    hFig, hAx, secondHand, minuteHand, hourHand, dial, dialText, dialCenter, dialTicks};
-t.TimerFcn = {@updateClock, hFig, hAx, secondHand, hourHand, minuteHand};
+    hFig, hAx, secondHand, minuteHand, hourHand, dial, dialText, dialCenter,...
+    dialTicks, hAlarm};
+t.TimerFcn = {@updateClock, hFig, hAx, secondHand, hourHand, minuteHand, alarmTime};
 start(t);
-
+hAlarm{1}.Callback = 
--- initializeGraphicHandles.m
+++ initializeGraphicHandles.m
@@ -1,4 +1,4 @@
-function [hFig, hAx, hToggle] = initializeGraphicHandles()
+function [hFig, hAx, hToggle, hAlarm] = initializeGraphicHandles()
     hFig = figure('Name', 'Karsh''s Clock');
     hFig.NumberTitle = 'off';
     hFig.MenuBar = 'None';
@@ -7,5 +7,23 @@ function [hFig, hAx, hToggle] = initializeGraphicHandles()
     hToggle.Units = 'normalized';
     hToggle.Position = [0.1 0.1 0.1 0.1];
     hAx.Units = 'normalized';
-    hAx.Position = [0.2 0.2 0.7 0.7];
+    hAx.Position = [0.2 0.25 0.7 0.7];
+    hAlarm = {uicontrol(hFig, 'Style', 'edit', 'Units', 'normalized', 'String', 'HH'), ...
+            uicontrol(hFig, 'Style', 'edit', 'Units', 'normalized', 'String', 'MM')};
+    for i = 1:length(hAlarm)
+        hAlarm{i}.FontName = 'Consolas';
+        hAlarm{i}.FontSize = 12;
+        hAlarm{i}.FontWeight = 'bold';
+    end
+    hold(hAx, 'on');
+    alarmText = uicontrol(hFig, 'Style', 'text');
+    alarmText.String = 'Alarm';
+    alarmText.Units = 'normalized';
+    alarmText.Position = [0.1, 0.40, 0.1, 0.1];
+    alarmText.FontName = 'Consolas';
+    alarmText.FontSize = 12;
+    alarmText.FontWeight = 'bold';
+    hold(hAx, 'off');
+    hAlarm{1}.Position = [0.1 0.3 0.1 0.1];
+    hAlarm{2}.Position = [0.2 0.3 0.1 0.1];
 end
